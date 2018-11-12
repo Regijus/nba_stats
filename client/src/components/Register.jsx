ac765fab3c9a5a3f61b1ffb43cafba0ad1105c41
Mantas Arlauskas <mantasarlauskas1997@gmail.com>
2018-11-12 22:56:27 +0200

    Added simple registration with validation

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -7,17 +7,92 @@ import {
   Input,
   Card,
   CardTitle,
-  CardBody
+  CardBody,
+  Alert
 } from "reactstrap";
 import '../styles/registerStyles.css';
+import { serverUrl } from '../config/server';
+import axios from 'axios';
 
 class Register extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      username: '',
+      password: '',
+      repeatPassword: '',
+      errorMessage: '',
+      successMessage: ''
+    };
+  }
+
+  setSuccess = message => {
+    this.setState({
+      errorMessage: '',
+      successMessage: message
+    });
+  };
+
+  setError = message => {
+    this.setState({
+      errorMessage: message,
+      successMessage: ''
+    });
+  };
+
+  validateForm = () => {
+    const { email, username, password, repeatPassword } = this.state;
+
+    if(email.length === 0 || username.length === 0 || password.length === 0 || repeatPassword === 0) {
+      this.setError('Fields must not be empty');
+      return false;
+    } else if(password !== repeatPassword) {
+      this.setError('Passwords must match');
+      return false;
+    } 
+
+    return true;
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value
+    })
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.validateForm() && this.postData();
+  };
+
+  postData = () => {
+    const { email, username, password, repeatPassword } = this.state;
+
+    axios
+      .post(`${serverUrl}/users`, {
+        email,
+        username,
+        password
+      })
+      .then(({ status, data }) => {
+        status === 201 && this.setSuccess(data);
+      });
+  };
+  
+
   render() {
+    const { errorMessage, successMessage } = this.state;
+
     return (
       <Card className="registerCard">
         <CardBody>
           <CardTitle className="registerHeader">Register</CardTitle>
-          <Form>
+          <Form onSubmit={this.handleSubmit}>
+            {errorMessage && <Alert color="danger">{errorMessage}</Alert>}
+            {successMessage && <Alert color="success">{successMessage}</Alert>}
             <FormGroup>
               <Label for="email">Email</Label>
               <Input
@@ -25,6 +100,7 @@ class Register extends Component {
                 name="email"
                 id="email"
                 placeholder="email"
+                onChange={this.handleChange}
               />
             </FormGroup>
             <FormGroup>
@@ -34,6 +110,7 @@ class Register extends Component {
                 name="username"
                 id="username"
                 placeholder="Username"
+                onChange={this.handleChange}
               />
             </FormGroup>
             <FormGroup>
@@ -43,6 +120,7 @@ class Register extends Component {
                 name="password"
                 id="password"
                 placeholder="Password"
+                onChange={this.handleChange}
               />
             </FormGroup>
             <FormGroup>
@@ -52,6 +130,7 @@ class Register extends Component {
                 name="repeatPassword"
                 id="repeatPassword"
                 placeholder="Repeat Password"
+                onChange={this.handleChange}
               />
             </FormGroup>
             <Button color="primary">Submit</Button>
--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongo = require('mongodb');
 const mongoClient = require('mongodb').MongoClient;
+const bcrypt = require('bcrypt');
 
 const app = express();
 
-
 // Bodyparser Middleware
 app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
 
 // DB Config
-const db = require('./config/keys').databaseURI;
+const url = require('./config/keys').databaseURI;
+let db;
 
-mongoClient.connect(db, { useNewUrlParser: true });
+mongoClient.connect(url, (err, database) => {
+	if (err) throw err;
+	db = database.db('nba-stats');
+	db.createCollection('users', (err, res) => {
+		if (err) throw err;
+		console.log('Collection created!');
+	});
+});
 
 // Use Routes
+app.post('/users', ({ body: { password, ...fields }}, res) => {
+	db.collection('users').insertOne({ ...fields, password: bcrypt.hashSync(password, 10)}, err => {
+		if (err) throw err;
+		res.status(201).json('Registration successful');
+	})
+});
+
+app.get('/users', (req, res) => {
+	db.collection('users').find().toArray((err, results) => {
+		console.log(results);
+	})
+});
 
 // Port Config
 const port = process.env.PORT || 5000;
