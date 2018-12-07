38dc4b9cc6b7739bfd19163c6e73b8120e6290ce
Mantas Arlauskas <mantasarlauskas1997@gmail.com>
2018-12-07 17:59:14 +0200

    Added new bodyparser

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const app = express();
 // Bodyparser Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
